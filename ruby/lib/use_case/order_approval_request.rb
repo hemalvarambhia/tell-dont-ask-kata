137044a4a5823d136ef84d696c1e9b5757b09def
hemalvarambhia <h.n.varambhia@gmail.com>
2023-01-03 16:00:30 +0000

    Now clients can pass the approval directly via the constructor.

--- ruby/lib/use_case/order_approval_request.rb
+++ ruby/lib/use_case/order_approval_request.rb
@@ -3,7 +3,8 @@
 class OrderApprovalRequest
   attr_accessor :order_id, :approved
 
-  def initialize(order_id: nil)
+  def initialize(order_id: nil, approved: nil)
     @order_id = order_id
+    @approved = approved
   end
 end
