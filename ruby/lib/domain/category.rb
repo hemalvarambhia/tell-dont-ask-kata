a228cd2494ea9e6c9b4a0c20989fe267a7656cc1
hemalvarambhia <h.n.varambhia@gmail.com>
2023-01-07 20:26:03 +0000

    No need for the default setting.

--- ruby/lib/domain/category.rb
+++ ruby/lib/domain/category.rb
@@ -3,7 +3,7 @@
 class Category
   attr_accessor :name, :tax_percentage
 
-  def initialize(name: '', tax_percentage: 0)
+  def initialize(name:, tax_percentage:)
     @name = name
     @tax_percentage = tax_percentage
   end
