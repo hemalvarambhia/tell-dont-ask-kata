a600c3f73f30c8127e5bd89740215dab8adcfd29
hemalvarambhia <h.n.varambhia@gmail.com>
2023-01-18 21:54:27 +0000

    Removed writer methods.

--- ruby/lib/use_case/order_approval_request.rb
+++ ruby/lib/use_case/order_approval_request.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OrderApprovalRequest
-  attr_accessor :order_id, :approved
+  attr_reader :order_id, :approved
 
   def initialize(order_id:, approved:)
     @order_id = order_id
