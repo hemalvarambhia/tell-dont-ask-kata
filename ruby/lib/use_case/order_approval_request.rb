e78e9efe5b1c95a96be8502fbd92aabba9175dc4
hemalvarambhia <h.n.varambhia@gmail.com>
2023-01-18 21:53:51 +0000

    Removed obsolete defaults.

--- ruby/lib/use_case/order_approval_request.rb
+++ ruby/lib/use_case/order_approval_request.rb
@@ -3,7 +3,7 @@
 class OrderApprovalRequest
   attr_accessor :order_id, :approved
 
-  def initialize(order_id: nil, approved: nil)
+  def initialize(order_id:, approved:)
     @order_id = order_id
     @approved = approved
   end
