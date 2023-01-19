5c17527b3ee65c1e31ef09936f12502c2dcd52cd
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-01-19 11:55:17 +0000

    Introduced read only methods.

--- ruby/lib/domain/order.rb
+++ ruby/lib/domain/order.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Order
-  attr_accessor :total, :currency, :items, :tax, :status, :id
+  attr_reader :total, :currency, :items, :tax, :status, :id
 
   def self.blank(id:, currency: 'EUR')
     created(id: id, total: 0.0, tax: 0.0, currency: currency, items: [])
